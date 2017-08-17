d72c30406121bdcaabfab5538898521673f95904
hpnhxxwn <nizhongy@umich.edu>
2017-08-16 19:04:48 -0700

    *

--- database.py
+++ database.py
@@ -6,23 +6,50 @@ import datetime
 import json
 
 
-#db = client.test_database
-
-#collection = db.test_collection
-
-#post = {"author": "Mike", "text": "My first blog post!", "tags": ["mongodb", "python", "pymongo"], "date": datetime.datetime.utcnow()}
-
-#posts = db.posts
-#post_id = posts.insert_one(post).inserted_id
-#post_id
-
-
 class DB():
+	'''
+	USERS_DATA schema:
+	_id = unique value
+	name = "string"
+	location = "string"
+	education = {json object with major, degree, and school }
+	job category = array of json objects
+					[{SW: {
+					  SW1: 1, 
+					  SW2: 0, 
+					  SW3: 0 
+					}},
+					 {PM:{
+					  PM1:,
+					  PM2:,
+					  PM3:
+					}},
+					 {Mkt
+					]
+					age_range  = [array eg values 20-30 etc ]
+					years of job_category = integer 
+					personality color = [array with blue, red, green ,yellow ]
+					Interests = string
+					skills = json object
+					requirements = "string"
+					method = "string"
+					programs = array of json objects
+					[{
+					 P1: 
+				
+					},
+					{
+					 P2:
+					}
+					]
+	is_mentor = boolean
+
+	'''
 
 	def __init__(self):
 		self.client = MongoClient('mongodb://localhost:27017/')
 		self.db = self.client.mentor_tool
-		self.user_table = self.db.user_table
+		self.user_table = self.db.USERS_DATA
 
 	def insertPost(self, user_data):
 		"""
@@ -32,16 +59,40 @@ class DB():
 			Insert JSON data into MongoDB
 
 		Converts input string to dictionary and insert it to the table.
+
+		user data format:
+		
+
 		"""
 
-		print(user_data)
+		#print(user_data)
+		#data = user_data.split(';')
+		#doc = {}
+		#education = data[3].split(',')
+		#job_category = data[4].split('')
+		#edu = {'major': education[0], 'degree': education[1], 'school': education[2]}
+		#doc['_id'] = data[0]
+		#doc['name'] = data[1]
+		#doc['location'] = data[2]
+		#doc['education'] = edu
+		#doc['job category']= {}
+
 		data = json.loads(user_data);
-		post_id = self.user_table.insert_one(data)
+		res = True
+		try:
+			post_id = self.user_table.insert_one(data)
+		except:
+			print("Duplicate key, insertion failed!")
+			res = False
+		finally:
+			if res:
+				print("Insertion success")
+			return
 
 	def readPost(self, user_id, attr=None):
 		"""
 		Input parameter: 
-			user_id: string
+			user_id: int
 			attr: attributes want to query along with user_id, must be a key value pair JSON
 		Output parameter: 
 			data associate with user_id
@@ -49,10 +100,10 @@ class DB():
 		Read data from DB by primary key user_id, and with other attributes if exist. Return None if not exist.
 		"""
 
-		if not checkExist(user_id):
+		if not self.checkExist(user_id):
 			return None
 
-		query_filter = {'user_id': user_id}
+		query_filter = {'_id': user_id}
 		if attr:
 			attr_json = json.loads(attr);
 			for key, value in attr_json:
@@ -65,29 +116,30 @@ class DB():
 	def deletePost(self, user_id):
 		"""
 		Input parameter:
-			user_id: string
+			user_id: int
 		Output parameter: 
 			void
 
 		Remove a document from DB by user_id if user_id exist.
 		"""
-		if not checkExist(user_id):
-			return
+		if not self.checkExist(user_id):
+			return False
 
-		query_filter = {'user_id': user_id}
+		query_filter = {'_id': user_id}
 		self.user_table.remove(query_filter)
+		return True
 
 	def checkExist(self, user_id):
 		""" 
 		Input parameter: 
-			user_id: string
+			user_id: int
 		Output parameter: 
 			boolean indicates if the user_id info exist or not
 
 		Check if the document exist in DB or not. Ture means exist, false means no.
 		"""
 
-		query_filter = {'user_id': user_id}
+		query_filter = {'_id': user_id}
 		cnt = self.user_table.find(query_filter).count()
 		if (cnt > 0):
 			return True
@@ -103,15 +155,21 @@ class DB():
 		boolean indicates if update is successful or not
 		"""
 
-		if not checkExist(user_id):
+		if not self.checkExist(user_id):
 			return False
 
-		data = json.loads(user_data)
-		input_data = {'user_id': user_id}
-		for key, value in data:
-			input_data[key] = value
+		query_filter = {}
+		query_filter['_id'] = user_id
+		input_data = {'$set': json.loads(user_data)}
 
-		self.user_table.update_one(input_data)
+		print(input_data)
+		#data = json.loads(user_data)
+		#data['_id'] = user_id
+		#print(data)
+		#for key, value in data:
+		#	input_data[key] = value
+
+		self.user_table.update(query_filter, input_data)
 
 		return True
 
@@ -119,19 +177,59 @@ if __name__ == "__main__":
 	db = DB()
 	user_id = 0
 	name = "miaomiao"
-	job_category = "SDE manager"
-	years = 5
-	skills = "Java, Python, AWS"
+	job_category = [{'SW': {'SW1': 1, 'SW2': 0, 'SW3': 0 }},{'PM':{'PM1': 0,'PM2': 2,'PM3': 0}}]
+	education = {'major': 'EE', 'degree':'MS', 'school':'University of Michigan'}
+	years_of_job_category = 5 
+	personality_color = ['red', 'blue']
+	Interests = "Music, Gaming"
+	age_range = "20-30"
+	skills = {'Java': 10, 'Python': 10, 'AWS': 10}
 	requirements = "public speaking, leadership, music"
 	method = 'online'
-	self_assesment = '10, 10, 10'
 	is_mentor = False
-	programs = "[]"
+	programs = [{}]
 	calendar = ""
 
-	data = "{'user_id': %d, 'name': %s, 'job category': %s, 'years for current job category': %d, 'skills': %s, 'requirements': %s, 'method': %s, 'self assesment': %s, 'is mentor': %s, 'programs': %s, 'calendar': %s}" \
-	% (user_id, name, job_category, years, skills, requirements, method, self_assesment, is_mentor, programs, calendar)
-	db.insertPost(data)
+	#data = "{'id': %d, 'name': %s, 'job category': %s, 'years for current job category': %d, 'skills': %s, 'requirements': %s, 'method': %s, 'self assesment': %s, 'is mentor': %s, 'programs': %s, 'calendar': %s}" \
+	#% (user_id, name, job_category, years, skills, requirements, method, self_assesment, is_mentor, programs, calendar)
+	data = {}
+	data['_id'] = user_id
+	data['name'] = name
+	data['job_category'] = job_category
+	data['education'] = education
+	data['years_of_job_category'] = 5
+	data['personality_color'] = personality_color
+	data['Interests'] = Interests
+	data['age_range'] = age_range
+	data['skills'] = skills
+	data['requirements'] = requirements
+	data['method'] = method
+	data['is_mentor'] = is_mentor
+	data['programs'] = programs
+
+	db.insertPost(json.dumps(data))
+
+	read_data = db.readPost(0)
+	if read_data is None:
+		print("data is none")
+	else:
+		print(read_data)
+
+	#if db.deletePost(0):
+	#	print("delete is successful")
+	#else:
+	#	print("hehe")
+
+	update_data = {}
+	update_data['job_category'] = job_category
+	programs = [{'prog': 'music', 'mentor': False, 'partener': 'YuanMiao'}]
+	update_data['programs'] = programs
+	if db.update(0, json.dumps(update_data)):
+		print("update is successful")
+	else:
+		print("Hehe")
+
+
 
 
 
